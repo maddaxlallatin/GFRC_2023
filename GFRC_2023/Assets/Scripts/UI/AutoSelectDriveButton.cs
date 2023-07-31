738a20c234fbd68a9915f54fa668ccfeb574a9eb
Maddax Lallatin <65627323+maddaxlallatin@users.noreply.github.com>
2023-07-31 16:33:25 -0400

    WORK

---
+++ GFRC_2023/Assets/Scripts/UI/AutoSelectDriveButton.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AutoSelectDriveButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Button>().Select();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
