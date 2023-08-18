a50f55a34e72deb4ca2edfd5f56f16ab70ad50aa
Pigeonman5 <adam.w.miladi@gmail.com>
2023-08-17 21:06:32 -0400

    push
    push

---
+++ GFRC_2023/Assets/Scenes/AdamMiladi/NPCManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static Global;
+
+public class NPCManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(manipulator);
+    }
+}
