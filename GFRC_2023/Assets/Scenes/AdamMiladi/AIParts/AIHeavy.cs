35a1d2a71ae63e120c3df50c487baf41ac892a09
Pigeonman5 <adam.w.miladi@gmail.com>
2023-08-19 04:08:18 -0400

    AI has control of it's rotation
    AI can now lifelessly stare into cubes at any degree on the x,z plane!

--- GFRC_2023/Assets/Scenes/AdamMiladi/AIParts/AIHeavy.cs
+++ GFRC_2023/Assets/Scenes/AdamMiladi/AIParts/AIHeavy.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 public class AIHeavy : MonoBehaviour
 {
     public GameObject FLift;
-
+    public float DrivePower = 8f;
     public AIGrabCone ConeCheck;
     public AIGrabCube CubeCheck;
     public bool Searched = false;
     public GameObject Closest;
+    public GameObject THexDrive;
+    public GameObject Drive;
     public float distance;
+
 
     public Wheel[] RWheels;
     public Wheel[] LWheels;
@@ -17,10 +20,17 @@ public class AIHeavy : MonoBehaviour
     public GameObject[] Cubes;
     ItemSpawner FieldObjects;
     public Vector3 Target;
+    public Vector3 Path;
+    public float RoTarget;
     // Start is called before the first frame update
     void Start()
     {
     FieldObjects = FindObjectOfType<ItemSpawner>();
+    // Drive = Instantiate(THexDrive,transform);
+    // RWheels = Drive.GetComponent<WheelArray>().RWheels;
+    // LWheels = Drive.GetComponent<WheelArray>().LWheels;
+
+
     }
 
     public Vector3 FindClosestCube()
@@ -41,11 +51,73 @@ public class AIHeavy : MonoBehaviour
         return Closest.transform.position;
     }
 
-    void Update()
+    void FixedUpdate()
     {
     Cubes = FieldObjects.FindCubes();
     Target = FindClosestCube();
-    Debug.Log(Target);
+    // Debug.Log(Target);
+    Path = Target - transform.position;
+    // Debug.Log(Path);
+    
+    if (Path.x < 0 && Path.z > 0)
+    {
+    RoTarget = 180-((180/Mathf.PI)*Mathf.Atan(Path.z/Path.x)+360);
+    }
+    else if (Path.x > 0)
+    {
+    RoTarget = 180-((180/Mathf.PI)*Mathf.Atan(Path.z/Path.x)+180);
+    }
+    else 
+    {
+    RoTarget = 180-(180/Mathf.PI)*Mathf.Atan(Path.z/Path.x);
+    }
+    // Debug.Log(RoTarget);
+    // Debug.Log(transform.eulerAngles.y);
+
+    if (transform.eulerAngles.y > RoTarget + .5f || transform.eulerAngles.y < RoTarget - .5f)
+    {
+    
+    if (transform.eulerAngles.y - RoTarget > 0)
+    {
+    foreach(Wheel w in RWheels)
+    {
+        w.Accelerate(DrivePower);
+        w.UpdatePosition();
+    }
+    foreach(Wheel w in LWheels)
+    {
+        w.Accelerate(-DrivePower);
+        w.UpdatePosition();
+    }
+    }
+    else
+    {
+    foreach(Wheel w in RWheels)
+        {
+            w.Accelerate(-DrivePower);
+            w.UpdatePosition();
+        }
+        foreach(Wheel w in LWheels)
+        {
+            w.Accelerate(DrivePower);
+            w.UpdatePosition();
+        }
+    }
+    }
+    else 
+    {
+    // Debug.Log("Victory");
+        foreach(Wheel w in RWheels)
+        {
+            w.Accelerate(0);
+            w.UpdatePosition();
+        }
+        foreach(Wheel w in LWheels)
+        {
+            w.Accelerate(0);
+            w.UpdatePosition();
+        }
+    }
     }
     // void Update()
     // {
---
+++ GFRC_2023/Assets/Scenes/AdamMiladi/AIParts/WheelArray.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelArray : MonoBehaviour
+{
+    public Wheel[] RWheels;
+    public Wheel[] LWheels;
+}
